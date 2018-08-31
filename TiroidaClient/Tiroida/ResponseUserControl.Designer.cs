b0a0ddcda3f6bb69a2a197851ea9635b04dfb821
FoxBlood72 <pantofgigel@yahoo.com>
2018-09-01 01:48:27 +0300

    Additional files for GUI

---
+++ TiroidaClient/Tiroida/ResponseUserControl.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Tiroida
+{
+    partial class ResponseUserControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResponseUserControl));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(256, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(409, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(26, 396);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(140, 25);
+            this.metroLabel1.TabIndex = 1;
+            this.metroLabel1.Text = "Sansa de cancer:";
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.AutoSize = true;
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(26, 560);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(163, 25);
+            this.metroLabel2.TabIndex = 2;
+            this.metroLabel2.Text = "Sansa de alta boala:";
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.AutoSize = true;
+            this.metroLabel3.Location = new System.Drawing.Point(419, 402);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel3.TabIndex = 3;
+            this.metroLabel3.Text = "metroLabel3";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.AutoSize = true;
+            this.metroLabel4.Location = new System.Drawing.Point(419, 566);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(83, 19);
+            this.metroLabel4.TabIndex = 4;
+            this.metroLabel4.Text = "metroLabel4";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(806, 577);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(95, 43);
+            this.metroButton2.TabIndex = 6;
+            this.metroButton2.Text = "Inapoi la panou";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // ResponseUserControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.metroLabel4);
+            this.Controls.Add(this.metroLabel3);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "ResponseUserControl";
+            this.Size = new System.Drawing.Size(904, 623);
+            this.Load += new System.EventHandler(this.ResponseUserControl_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
---
+++ TiroidaClient/Tiroida/ResponseUserControl.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tiroida
+{
+    public partial class ResponseUserControl : UserControl
+    {
+        delegate void SetInterfaceCallBack(UserControl usercontrol);
+
+
+        private void SetInterface(UserControl usercontrol)
+        {
+            FlowLayoutPanel panel = (FlowLayoutPanel)this.Parent;
+            if (panel.InvokeRequired)
+            {
+                SetInterfaceCallBack callback = new SetInterfaceCallBack(SetInterface);
+                this.Invoke(callback, new object[] { usercontrol });
+            }
+            else
+            {
+                panel.Controls.Clear();
+                panel.Controls.Add(usercontrol);
+            }
+        }
+
+        public ResponseUserControl()
+        {
+            InitializeComponent();
+        }
+
+        private void ResponseUserControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        internal void SetCancer(string s)
+        {
+            this.metroLabel3.Text = s;
+        }
+
+
+        internal void SetNonCancer(string s)
+        {
+            this.metroLabel4.Text = s; 
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            PersoanlDataForm form = new PersoanlDataForm();
+            SetInterface(form);
+        }
+    }
+}
